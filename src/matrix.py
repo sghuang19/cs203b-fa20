de041b70554d91d501d0d95f560290d53dc659eb
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-17 00:29:37 +0800

    Create matrix.py

---
+++ src/matrix.py
@@ -0,0 +1,10 @@
+def square_matrix_multiply(self, a, b):
+    """Use the standard approach to multiply 2 matrices a and b, return their product."""
+    # TODO(SamuelHuang2019): Finish the docstring.
+    print('Hello World!')
+
+
+class Matrix:
+    """A data structure for matrix."""
+    # TODO(SamuelHuang2019): Finish the docstring.
+    # TODO(SamuelHuang2019): More methods.
