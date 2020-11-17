51d9461ea12e5072669c5800ced143b3b83878cd
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-17 08:54:11 +0800

    Update class doc string

--- src/matrix.py
+++ src/matrix.py
@@ -5,6 +5,9 @@ def square_matrix_multiply(self, a, b):
 
 
 class Matrix:
-    """A data structure for matrix."""
+    """
+    A data structure for matrix.
+    The matrix implementation should be suitable for dense matrices. You are required to define a class Matrix that will be used in the implementation. This class Matrix will represent a matrix in “row-major” order (i.e. for an n×n-matrix the first row will be stored in an array at index 0 to index n-1 the next row at n to 2n-1 and so on). The class should provide a constructor and methods to get and set the element at any row column index.
+    """
     # TODO(SamuelHuang2019): Finish the docstring.
     # TODO(SamuelHuang2019): More methods.
