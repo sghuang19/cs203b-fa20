ccb7bffea7a306e777371be4a24a9890155f49e7
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-21 11:39:00 +0800

    Update test.py

--- src/test/test.py
+++ src/test/test.py
@@ -10,3 +10,9 @@ p = Person('Swaroop')
 p.say_hi()
 # The previous 2 lines can also be written as
 # Person('Swaroop').say_hi()
+
+str = 'abcdefghijklmnopqrstuvwxyz'
+s = slice(5, 20, 2)
+i = s.indices(10)
+print(i)
+print(str[1:3])
