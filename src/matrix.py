2eaa1524e886660f9ea83d49d8e1cf851a2eaaa3
shuhan <shuhan.zhengphy@gmail.com>
2020-11-22 23:15:12 +0800

    Add standard square_matrix_multiply method Add Strassen mul method Add adaptiveadd and adaptiveminus methods Modified getitem

--- src/matrix.py
+++ src/matrix.py
@@ -1,4 +1,5 @@
 import exceptions
+import math
 
 
 def square_matrix_multiply(a, b):
@@ -9,8 +10,157 @@ def square_matrix_multiply(a, b):
     :type b: Matrix
     :return: Matrix
     """
-    # TODO(SamuelHuang2019): Finish the docstring.
-    print('Hello World!')
+    if a.column != b.row:
+        print("Unmatched matrix size")
+        return
+    s = [0] * (a.row * b.column)
+    for i in range(a.row):
+        for j in range(b.column):
+            for k in range(a.column):
+                s[i * b.column + j] = s[i * b.column + j] + a[i + 1, k + 1] * b[k + 1, j + 1]
+    c = Matrix(s, a.row, b.column)
+    return c
+
+
+def strassen_mutliply(a, b):  # for almost square matrix
+    """
+    Use the Strassen to multiply 2 matrices a and b, return their product.
+
+    :type a: Matrix
+    :type b: Matrix
+    :return: Matrix
+    """
+    arow = a.row
+    acol = a.column
+    brow = b.row
+    bcol = b.column
+    if acol != brow:
+        print("Unmatched matrix size")
+        return
+
+    if min(arow, acol, bcol) < 3:
+        return square_matrix_multiply(a, b)
+
+    half_arow_ceil = math.ceil(arow / 2)
+    half_arow_floor = math.floor(arow / 2)
+    half_acol_ceil = math.ceil(acol / 2)
+    half_acol_floor = math.floor(acol / 2)
+
+    half_brow_ceil = math.ceil(brow / 2)
+    half_brow_floor = math.floor(brow / 2)
+    half_bcol_ceil = math.ceil(bcol / 2)
+    half_bcol_floor = math.floor(bcol / 2)
+
+    A0 = a[1:half_arow_ceil, 1:half_acol_ceil]
+    A1 = a[1:half_arow_ceil, half_acol_ceil + 1:half_acol_ceil + half_acol_floor]
+    A2 = a[half_arow_ceil + 1:half_arow_ceil + half_arow_floor, 1:half_acol_ceil]
+    A3 = a[half_arow_ceil + 1:half_arow_ceil + half_arow_floor, half_acol_ceil + 1:half_acol_ceil + half_acol_floor]
+    B0 = b[1:half_brow_ceil, 1:half_bcol_ceil]
+    B1 = b[1:half_brow_ceil, half_bcol_ceil + 1:half_bcol_ceil + half_bcol_floor]
+    B2 = b[half_brow_ceil + 1:half_brow_ceil + half_brow_floor, 1:half_bcol_ceil]
+    B3 = b[half_brow_ceil + 1:half_brow_ceil + half_brow_floor, half_bcol_ceil + 1:half_bcol_ceil + half_bcol_floor]
+
+    T2 = adaptiveminus(B1, B3, half_bcol_ceil, half_bcol_floor)
+    M3 = strassen_mutliply(A0, T2)
+    C1 = M3
+    C3 = adaptiveadd(M3, Matrix([0], 1, 1), half_arow_floor, half_bcol_floor)
+
+    T1 = adaptiveminus(A2, A0, half_arow_ceil, half_acol_ceil)
+    T2 = adaptiveadd(B0, B1, half_bcol_ceil, half_bcol_ceil)
+    M6 = strassen_mutliply(T1, T2)
+    C3 = adaptiveadd(C3, M6, half_arow_floor, half_bcol_floor)
+
+    T1 = adaptiveadd(A2, A3, half_arow_floor, half_acol_ceil)
+    M2 = strassen_mutliply(T1, B0)
+    C2 = M2
+    C3 = adaptiveminus(C3, M2, half_arow_floor, half_bcol_floor)
+
+    T1 = adaptiveadd(A0, A3, half_arow_ceil, half_acol_ceil)
+    T2 = adaptiveadd(B0, B3, half_brow_ceil, half_bcol_ceil)
+    M1 = strassen_mutliply(T1, T2)
+    C0 = M1
+    C3 = adaptiveadd(C3, M1, half_arow_floor, half_bcol_floor)
+
+    T1 = adaptiveadd(A0, A1, half_arow_ceil, half_acol_floor)
+    M5 = strassen_mutliply(T1, B3)
+    C0 = adaptiveminus(C0, M5, half_arow_ceil, half_bcol_ceil)
+    C1 = adaptiveadd(C1, M5, half_arow_ceil, half_bcol_floor)
+
+    T1 = adaptiveminus(A1, A3, half_arow_ceil, half_acol_floor)
+    T2 = adaptiveadd(B2, B3, half_brow_floor, half_bcol_ceil)
+    M7 = strassen_mutliply(T1, T2)
+    C0 = adaptiveadd(C0, M7, half_arow_ceil, half_bcol_ceil)
+
+    T2 = adaptiveminus(B2, B0, half_brow_floor, half_bcol_ceil)
+    M4 = strassen_mutliply(A3, T2)
+    C0 = adaptiveadd(C0, M4, half_arow_ceil, half_bcol_ceil)
+    C2 = adaptiveadd(C2, M4, half_arow_floor, half_bcol_ceil)
+
+    s = []
+    for i in range(C0.row):
+        s = s + C0[i + 1:i + 1, 1:C0.column].elements + C1[i + 1:i + 1, 1:C1.column].elements
+    for j in range(C2.row):
+        s = s + C2[j + 1:j + 1, 1:C2.column].elements + C3[j + 1:j + 1, 1:C3.column].elements
+    c = Matrix(s, a.row, b.column)
+    return c
+
+
+def adaptiveadd(a, b, target_row, target_col):
+    """
+        Given target matrix size, perform adaptive matrix addition
+        :type a: Matrix
+        :type b: Matrix
+        :type target_col: Integer
+        :type target_row: Integer
+        :return: Matrix
+        """
+    arow = a.row
+    acol = a.column
+    brow = b.row
+    bcol = b.column
+    s = [0] * (target_col * target_row)
+    for i in range(target_row):
+        for j in range(target_col):
+            flag = False
+            if 0 <= i < arow and 0 <= j < acol:
+                s[i * target_col + j] = a[i + 1, j + 1]
+                flag = True
+            if 0 <= i < brow and 0 <= j < bcol:
+                s[i * target_col + j] = s[i * target_col + j] + b[i + 1, j + 1]
+                flag = True
+            if not flag:
+                s[i * target_col + j] = 0
+    c = Matrix(s, target_row, target_col)
+    return c
+
+
+def adaptiveminus(a, b, target_row, target_col):
+    """
+        Given target matrix size, perform adaptive matrix subtraction
+        :type a: Matrix
+        :type b: Matrix
+        :type target_col: Integer
+        :type target_row: Integer
+        :return: Matrix
+        """
+    arow = a.row
+    acol = a.column
+    brow = b.row
+    bcol = b.column
+    s = [0] * (target_col * target_row)
+    for i in range(target_row):
+        for j in range(target_col):
+            flag = False
+            if 0 <= i < arow and 0 <= j < acol:
+                s[i * target_col + j] = a[i + 1, j + 1]
+                flag = True
+            if 0 <= i < brow and 0 <= j < bcol:
+                s[i * target_col + j] = s[i * target_col + j] - b[i + 1, j + 1]
+                flag = True
+            if not flag:
+                s[i * target_col + j] = 0
+    c = Matrix(s, target_row, target_col)
+    return c
 
 
 class Matrix:
@@ -93,28 +243,26 @@ class Matrix:
                 return Matrix(e, r, c)
 
             # one slice, one int
-            if isinstance(item[0], slice) and isinstance(item[1], slice):
-                return
 
             # one slice, one int
-            if isinstance(item[0], slice) and isinstance(item[1], slice):
-                return
 
             # both of the elements in the item are int
-            if item[0] > self.row or item[0] <= 0:
-                raise (exceptions.RowOutOfBoundsException(item))
-            if item[1] > self.column or item[1] <= 0:
-                raise (exceptions.ColumnOutOfBoundsException(item))
-            return self.elements[(item[0] - 1) * self.column + item[1] - 1]
+            if isinstance(item[0], int) and isinstance(item[1], int):
+                if item[0] > self.row or item[0] <= 0:
+                    raise (exceptions.RowOutOfBoundsException(item))
+                if item[1] > self.column or item[1] <= 0:
+                    raise (exceptions.ColumnOutOfBoundsException(item))
+                return self.elements[(item[0] - 1) * self.column + item[1] - 1]
 
-        # for slice input
-        if isinstance(item, slice):
-            return self.elements[item]
+        # # for slice input
+        # if isinstance(item, slice):
+        #     return self.elements[item]
 
         # else for int input
-        if item >= len(self.elements):
-            raise (exceptions.ColumnOutOfBoundsException(item))
-        return self.elements[item]
+        if isinstance(item, int):
+            if item > len(self.elements):
+                raise (exceptions.ColumnOutOfBoundsException(item))
+            return self.elements[item - 1]
 
     def __add__(self, other):
         """
@@ -133,6 +281,24 @@ class Matrix:
 
         for i in range(len(self.elements)):
             self.elements[i] += other
+        return self
+
+    def __sub__(self, other):
+        """
+        If other is a matrix, perform matrix subtraction, else perform subtraction with a number element-wisely
+
+        :rtype: Matrix
+        """
+        if type(other) is Matrix:
+            if self.row is other.row and self.column is other.column:
+                s = []
+                for i in range(len(self.elements)):
+                    s += [self.elements[i] - other.elements[i]]
+                return Matrix(s, self.row, self.column)
+            raise (exceptions.DimensionInconsistentException())
+
+        for i in range(len(self.elements)):
+            self.elements[i] -= other
         return self
 
     def __sizeof__(self):
--- src/test/matrix_gen_test.py
+++ src/test/matrix_gen_test.py
@@ -1,31 +1,16 @@
 from matrix import Matrix
-from matrix import square_matrix_multiply
+from matrix import strassen_mutliply
+import numpy as np
 
 elements = [1, 2, 3, 4, 5, 6]
-ma = Matrix(elements, 3, 2)
-print(ma.elements)
-elements = [1, 0, 1, 0]
-ma = Matrix(elements, 2, 2)
-print(ma)
-print(ma[0])
-print(ma[1, 1])
-print(ma[3])
-print(ma[2, 1])
+a1 = np.array(range(16)).reshape(4,4)
+a2 = np.array(range(16)).reshape(4,4)
 
-m1 = Matrix([1, 0, 1, 0], 2, 2)
-m2 = Matrix([0, 1, 0, 1], 2, 2)
-m3 = m1 + m2
-print(m3)
-print('==========')
-m4 = m3 + 1
-print(m4)
-print('==========')
-m5 = Matrix([1, 1, 1, 1], 1, 3)
-# print(m4 + m5)
-print('==========')
+m1 = Matrix(range(16),4,4)
+m2 = Matrix(range(16),4,4)
+#print(list(m1[1:1,1:m1.column]))
+c = strassen_mutliply(m1,m2)
+c2 = np.dot(a1,a2)
+print(c)
+print(c2)
 
-print('==========')
-
-mb = Matrix([1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5], 3)
-print(mb)
-print(mb[1:2, 1:2])
