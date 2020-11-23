3adb8ac2fc136af3a123ff699ddf871afb0a03a6
joshua-shuhan <54022159+joshua-shuhan@users.noreply.github.com>
2020-11-23 20:37:23 +0800

    Update matrix_gen_test.py

--- src/test/matrix_gen_test.py
+++ src/test/matrix_gen_test.py
@@ -14,3 +14,27 @@ c2 = np.dot(a1,a2)
 print(c)
 print(c2)
 
+
+from matrix import Matrix
+from matrix import strassen_mutliply
+from matrix import square_matrix_multiply
+import numpy as np
+import time
+
+
+m1 = Matrix(range(10000),100,100)
+m2 = Matrix(range(10000),100,100)
+#print(list(m1[1:1,1:m1.column]))
+time1 = time.time()
+c = strassen_mutliply(m1,m2)
+time2 = time.time()
+print(time2-time1)
+
+time1 =time.time()
+c1 = square_matrix_multiply(m1,m2)
+time2 =time.time()
+print(time2-time1)
+
+print(c1 - c)
+print(c1)
+print(c)
