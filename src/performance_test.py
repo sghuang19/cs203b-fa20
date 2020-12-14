c64f08acfbeff5578db5a7d728cc10f0a38af191
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-12-14 20:32:26 +0800

    Update performance_test.py

--- src/performance_test.py
+++ src/performance_test.py
@@ -1,25 +1,43 @@
-from  matrix import Matrix, square_matrix_multiply, strassen_multiply
+from matrix import *
 import time
 import random
 
-n = 512
+n = 10000
 
-elements = []
-for i in range(n * n):
-    elements.append(random.uniform(-1, 1))
-m1 = Matrix(elements, n, n)
+m1 = random_matrix_gen(n)
+m2 = random_matrix_gen(n)
 
-elements = []
-for i in range(n * n):
-    elements.append(random.uniform(-1, 1))
-m2 = Matrix(elements, n, n)
-
+e1 = []
+e2 = []
+s = 0
+for i in range(n):
+    e1.append(random.uniform(-1, 1))
+    e2.append(random.uniform(-1, 1))
 time1 = time.time()
-c = strassen_multiply(m1, m2)
+for i in range(n):
+    s = e1[i] + e2[i]
 time2 = time.time()
+alpha = time2 - time1
 print(time2 - time1)
 
 time1 = time.time()
-c1 = square_matrix_multiply(m1, m2)
+for i in range(n):
+    s = e1[i] * e2[i]
 time2 = time.time()
+pi = time2 - time1
 print(time2 - time1)
+
+print(alpha / pi)
+
+# m1 = random_matrix_gen(n)
+# m2 = random_matrix_gen(n)
+
+# time1 = time.time()
+# c = strassen_multiply(m1, m2)
+# time2 = time.time()
+# print(time2 - time1)
+#
+# time1 = time.time()
+# c1 = square_matrix_multiply(m1, m2)
+# time2 = time.time()
+# print(time2 - time1)
