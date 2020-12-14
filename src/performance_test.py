c77f8027d6873e6c5e7928a4c81188d87a81c4fb
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-12-14 22:39:03 +0800

    Update performance_test.py

--- src/performance_test.py
+++ src/performance_test.py
@@ -1,43 +1,17 @@
 from matrix import *
 import time
-import random
 
-n = 100000000000
+n = 1024
 
 m1 = random_matrix_gen(n)
 m2 = random_matrix_gen(n)
 
-e1 = []
-e2 = []
-s = 0
-for i in range(n):
-    e1.append(random.uniform(-1, 1))
-    e2.append(random.uniform(-1, 1))
 time1 = time.time()
-for i in range(n):
-    s = e1[i] + e2[i]
+c = strassen_multiply(m1, m2, 18)
 time2 = time.time()
-alpha = time2 - time1
 print(time2 - time1)
 
 time1 = time.time()
-for i in range(n):
-    s = e1[i] * e2[i]
+c1 = square_matrix_multiply(m1, m2)
 time2 = time.time()
-pi = time2 - time1
 print(time2 - time1)
-
-print(alpha / pi)
-
-# m1 = random_matrix_gen(n)
-# m2 = random_matrix_gen(n)
-
-# time1 = time.time()
-# c = strassen_multiply(m1, m2)
-# time2 = time.time()
-# print(time2 - time1)
-#
-# time1 = time.time()
-# c1 = square_matrix_multiply(m1, m2)
-# time2 = time.time()
-# print(time2 - time1)
