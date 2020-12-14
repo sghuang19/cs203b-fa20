add4ff147a7568b58b27265dc7ea4998b8395c47
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-12-15 00:44:46 +0800

    Rename function

--- src/test/matrix_gen_test.py
+++ src/test/matrix_gen_test.py
@@ -1,5 +1,5 @@
 from matrix import Matrix, square_matrix_multiply
-from matrix import strassen_mutliply
+from matrix import strassen_multiply
 import numpy as np
 
 elements = [1, 2, 3, 4, 5, 6]
@@ -9,7 +9,7 @@ a2 = np.array(range(16)).reshape(4, 4)
 m1 = Matrix(range(16), 4, 4)
 m2 = Matrix(range(16), 4, 4)
 # print(list(m1[1:1,1:m1.column]))
-c = strassen_mutliply(m1, m2)
+c = strassen_multiply(m1, m2)
 c2 = np.dot(a1, a2)
 print(c)
 print(c2)
@@ -19,7 +19,7 @@ m1 = Matrix(range(10000), 100, 100)
 m2 = Matrix(range(10000), 100, 100)
 # print(list(m1[1:1,1:m1.column]))
 # time1 = time.time()
-c = strassen_mutliply(m1, m2)
+c = strassen_multiply(m1, m2)
 # time2 = time.time()
 # print(time2-time1)
 
