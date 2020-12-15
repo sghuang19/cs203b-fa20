167f05c57edc07f0abd85eeca424e6e2d7733207
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-12-15 21:27:49 +0800

    Final adjust

--- src/crossover_point.py
+++ src/crossover_point.py
@@ -3,9 +3,10 @@ import time
 import random
 import openpyxl
 
-n = 128
-s = 35
-e = 45
+n = 512
+s = 1
+e = 64
+r = 2
 
 elements = []
 for i in range(n * n):
@@ -19,25 +20,31 @@ m2 = Matrix(elements, n, n)
 
 wb = openpyxl.load_workbook('data.xlsx')
 print("workbook", wb.sheetnames, "loaded")
-# ws = workbook['test']
-ws = wb.active
+ws = wb['crossover_point']
 
-for i in range(35, 45):
+for i in range(s, e + 1):
+    strassen = 0
+    square = 0
     ws['A' + str(i)] = i
-    print("==========")
     print("The recursion point is set to be ", i)
-    time1 = time.time()
-    c = strassen_multiply(m1, m2)
-    time2 = time.time()
-    print("The run time of Strassen's method is", time2 - time1)
-    ws['B' + str(i)] = time2 - time1
 
-    time1 = time.time()
-    c1 = square_matrix_multiply(m1, m2)
-    time2 = time.time()
-    print("The run time of brutal method is", time2 - time1)
-    ws['C' + str(i)] = time2 - time1
+    for j in range(1, r + 1):
+        print("test ", j)
 
+        time1 = time.time()
+        c = strassen_multiply(m1, m2)
+        time2 = time.time()
+        strassen = (time2 - time1 + strassen) / 2
+        print("The run time of Strassen's method is", strassen)
+
+        time1 = time.time()
+        c1 = square_matrix_multiply(m1, m2)
+        time2 = time.time()
+        square = (time2 - time1 + square) / 2
+        print("The run time of brutal method is", square)
+
+    ws['B' + str(i)] = strassen
+    ws['C' + str(i)] = square
     print("==========")
 
 wb.save('data.xlsx')
