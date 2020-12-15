46347e461a2684b136f8743dbedfb79c0fab017f
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-12-15 17:54:10 +0800

    Add code stop

--- src/flo_test.py
+++ src/flo_test.py
@@ -1,21 +1,21 @@
 import time
+import winsound
 
 
-def flo_test(m1, m2, n):
-    print("benchmark starts")
+def flo_addition_test(m1, m2, n):
+    print("addition benchmark starts")
 
     time1 = time.time()
     for i in range(n * n):
         s = m1[i] + m2[i]
     time2 = time.time()
-    alpha = time2 - time1
     print(time2 - time1)
 
+
+def flo_multiplication_test(m1, m2, n):
+    print("multiplication benchmark starts")
     time1 = time.time()
     for i in range(n * n):
         s = m1[i] * m2[i]
     time2 = time.time()
-    pi = time2 - time1
     print(time2 - time1)
-
-    print("the ratio of alpha and pi is ", pi / alpha)
--- src/flo_test_main.py
+++ src/flo_test_main.py
@@ -1,7 +1,24 @@
 from matrix import random_matrix_gen
-from flo_test import flo_test
+from flo_test import flo_addition_test, flo_multiplication_test
+import winsound
+from msvcrt import getch
 
-n = 8192 * 2
+# n = 8192
+n = 20
 m1 = random_matrix_gen(n)
+print("Matrix 1 generation completed")
 m2 = random_matrix_gen(n)
-flo_test(m1, m2, n)
+print("Matrix 2 generation completed, press any key to continue")
+winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+getch()
+
+print("addition test started")
+flo_addition_test(m1, m2, n)
+winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+print("addition test furnished, press any key to continue")
+getch()
+
+print("multiplication test started")
+flo_multiplication_test(m1, m2, n)
+winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+print("addition test furnished, press any key to exit")
